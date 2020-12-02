bf5aa2dd1d142ff3b240cda423cd6c2905de83e6
Chris Keeley <chrisgkeeley@gmail.com>
2020-12-02 16:30:44 +0000

    More idiomatic

--- src/main/kotlin/Advent1.kts
+++ src/main/kotlin/Advent1.kts
@@ -1,24 +1,22 @@
 import java.nio.file.Files
 import java.nio.file.Paths
 
-fun findEntries(expenses: List<Int>): Pair<Int, Int> {
+fun findEntriesSummingTo2020(expenses: List<Int>): Pair<Int, Int>? {
 
-    val whatever: List<Pair<Int, Int>> = expenses.mapNotNull {
-        val a = it
-        val found: Int? = expenses.find { it + a == 2020 }
-        if (found != null) {
-            Pair(a, found)
-        } else {
-            null
-        }
+    return expenses.find { expense ->
+        val complement: Int? = expenses.find { it + expense == 2020 }
+        complement != null
+    }?.let {
+        Pair(it, 2020 - it)
     }
-
-    return whatever.first()
 }
 // Laziness
 val input: List<Int> = Files.readAllLines(
     Paths.get("/home/chris/code/advent/src/main/resources/day1input.txt")
 ).map { it.toInt() }
 
-val result = findEntries(input)
-result.first * result.second
+val result = findEntriesSummingTo2020(input)
+println(result)
+if (result != null) {
+    println(result.first * result.second)
+}

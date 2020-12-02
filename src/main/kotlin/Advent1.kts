0a7194925e497fc90fc98d88d93c436c6d512c43
Chris Keeley <chrisgkeeley@gmail.com>
2020-12-02 15:09:02 +0000

    1: Naive solution

--- src/main/kotlin/Advent1.kts
+++ src/main/kotlin/Advent1.kts
@@ -1,6 +1,24 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+
 fun findEntries(expenses: List<Int>): Pair<Int, Int> {
 
+    val whatever: List<Pair<Int, Int>> = expenses.mapNotNull {
+        val a = it
+        val found: Int? = expenses.find { it + a == 2020 }
+        if (found != null) {
+            Pair(a, found)
+        } else {
+            null
+        }
+    }
 
+    return whatever.first()
 }
+// Laziness
+val input: List<Int> = Files.readAllLines(
+    Paths.get("/home/chris/code/advent/src/main/resources/day1input.txt")
+).map { it.toInt() }
 
-findEntries(listOf(1, 2, 2020))
+val result = findEntries(input)
+result.first * result.second

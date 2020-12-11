8a41fff7bc89ef61b385beadf67c1963af8adac4
Chris Keeley <chrisgkeeley@gmail.com>
2020-12-11 17:54:41 +0000

    6a

---
+++ src/main/kotlin/Advent6.kt
@@ -0,0 +1,35 @@
+package advent6
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+val input: String = Files.readString(
+    Paths.get("/home/chris/code/advent/src/main/resources/6.txt")
+)!!
+
+data class Group(val people: List<Person>){
+
+    fun groupQuestions(): Set<Char> {
+        return people.map{it.questions}.reduce{ allQuestions, newQuestions ->
+            allQuestions.union(newQuestions)
+        }
+    }
+}
+data class Person(val questions: Set<Char>)
+
+fun parseGroup(groupString: String): Group {
+    val people = groupString.split(Regex("""\n""")).map { parsePerson(it) }
+    return Group(people)
+}
+
+fun parsePerson(personString: String): Person {
+    val questions = personString.toCharArray().toSet()
+    return Person(questions)
+}
+
+fun main() {
+    val groups = input.split("\n\n").map { parseGroup(it) }
+
+    println(groups.sumBy { it.groupQuestions().size })
+
+}
